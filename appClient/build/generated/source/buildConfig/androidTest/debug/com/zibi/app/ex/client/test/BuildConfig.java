3b7a67c6240948711644923e9d063d665511dd84
DESKTOP-H6K4R8U\Zibi <z.sudnik@gmail.com>
2024-04-12 09:43:26 +0100

    clean

---
+++ appClient/build/generated/source/buildConfig/androidTest/debug/com/zibi/app/ex/client/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zibi.app.ex.client.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.zibi.client.test";
+  public static final String BUILD_TYPE = "debug";
+}
